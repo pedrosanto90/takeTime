8c4dbff67ea39a6801cfadef84830944186a7d48
pedrosanto90 <pedro.bb.90@gmail.com>
2024-03-23 15:22:20 +0000

    implementing the function to make countdown in correct format {minutes}:{seconds}

--- takeTime.py
+++ takeTime.py
@@ -3,17 +3,48 @@ import sys
 import os
 from time import sleep
 
-try:
-    time = int(sys.argv[1])
-    print(f'Taking {time}')
-    sleep(2)
+def argToTime(t):
+    os.system('clear')
 
-    for i in range(time):
+    minutes = t - 1
+    seconds = 59
+
+    for i in range(seconds):
         os.system('clear')
 
-        actualTime = time - i
-        print(actualTime)
+        if minutes < 10 and seconds < 10:
+            print(f'0{minutes}:0{seconds}')
+        elif minutes < 10:
+            print(f'0{minutes}:{seconds}')
+        elif seconds < 0:
+            print(f'{minutes}:0{seconds}')
+        else:
+            print(f'{minutes}:{seconds}')
+
+
+        seconds -= 1
+
+        if seconds == 0:
+            minutes -= 1
+            seconds = 59
+
+        elif minutes == 0 and seconds == 0:
+            print('END')
+
+            minutes = 0
+            seconds = 0
+
         sleep(1)
+
+
+time = int(sys.argv[1])
+
+try:
+    print(f'Taking {time}')
+    sleep(2)
+    
+    argToTime(time)
+
 
 except:
     print("No command line arguments passed")
